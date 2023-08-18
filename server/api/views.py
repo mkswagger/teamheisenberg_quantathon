177921fa666cc3b7442b63847e1bf184761399a1
ishaan <ishaankhurana123@gmail.com>
2023-08-19 04:55:13 +0530

    feat: initialized gen charts route

--- server/api/views.py
+++ server/api/views.py
@@ -10,6 +10,7 @@ from qiskit.circuit.library import TwoLocal
 from qiskit.opflow import PauliSumOp
 import matplotlib
 import matplotlib.pyplot as plt
+import time
 from qiskit.visualization import plot_bloch_multivector
 matplotlib.use('Agg')
 
@@ -47,14 +48,18 @@ def index(request):
         # Run the VQE algorithm
         result = vqe.compute_minimum_eigenvalue(operator=hamiltonian)
 
+
         # Extract the eigenstate
         eigenstate = result.eigenstate
 
         # Plot the eigenstate values as a wave
+        eigenvalues = result.eigenvalue
         plt.figure(figsize=(10, 6))
         plt.plot(np.real(eigenstate), label="Real Part", color="b")
         plt.plot(np.imag(eigenstate), label="Imaginary Part", color="r")
         plt.xlabel("Basis State")
+
+
         plt.ylabel("Value")
         plt.title("Eigenstate Waveform")
         plt.legend()
@@ -69,3 +74,108 @@ def index(request):
         plt.savefig(bloch_path)
 
         return Response({"message":"successful","bloch_path":bloch_path,"eigenstate_path":eigenstate_path},status=status.HTTP_200_OK)
+    
+@api_view(["POST"])
+def generate_charts(request):
+    if request.method == "POST":
+        hamiltonian = sum(
+            coeff * PauliSumOp.from_list([(term, coeff)]) for coeff, term in hamiltonian_terms
+        )
+        # Define the ansatz circuit
+        num_assets = 4  # Adjust as needed
+        ansatz = TwoLocal(num_assets, "ry", "cz", reps=3, entanglement="full")
+
+        # Initialize the VQE optimizer
+        optimizer = SPSA(maxiter=100)
+
+        # Create the VQE instance
+        vqe = VQE(
+            ansatz=ansatz,
+            optimizer=optimizer,
+            quantum_instance=AerSimulator(method="statevector"),
+        )
+
+        # Run the VQE algorithm
+        result = vqe.compute_minimum_eigenvalue(operator=hamiltonian)
+
+
+        # Extract the eigenstate
+        eigenstate = result.eigenstate
+
+        # Plot the eigenstate values as a wave
+        eigenvalues = result.eigenvalue
+        plt.figure(figsize=(10, 6))
+        plt.plot(np.real(eigenstate), label="Real Part", color="b")
+        plt.plot(np.imag(eigenstate), label="Imaginary Part", color="r")
+        plt.xlabel("Basis State")
+
+
+        plt.ylabel("Value")
+        plt.title("Eigenstate Waveform")
+        plt.legend()
+        plt.grid(True)
+        plt.tight_layout()
+        eigenstate_path = "media/plots/eigenstate_waveform.png"
+        plt.savefig(eigenstate_path)
+
+        state_vector = result.eigenstate
+        plot_bloch_multivector(state_vector)
+        bloch_path = "media/plots/bloch_sphere.png"
+        plt.savefig(bloch_path)
+
+        return Response({"message":"successful","bloch_path":bloch_path,"eigenstate_path":eigenstate_path},status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def benchmark(request):
+    data = request.data.get('data',[])
+    hamiltonian_terms = [(item['value'], item['term']) for item in data]
+
+    # Create the Hamiltonian operator
+    hamiltonian = sum(
+        coeff * PauliSumOp.from_list([(term, coeff)]) for coeff, term in hamiltonian_terms
+    )
+
+    # Define the ansatz circuit
+    num_assets = 4  # Adjust as needed
+    ansatz = TwoLocal(num_assets, "ry", "cz", reps=3, entanglement="full")
+
+    # Initialize the VQE optimizer
+    optimizer = SPSA(maxiter=100)
+
+    # Create the VQE instance
+    vqe = VQE(
+        ansatz=ansatz,
+        optimizer=optimizer,
+        quantum_instance=AerSimulator(method="statevector"),
+    )
+
+    # Measure the time it takes to perform the classical computation
+    start_time_classical = time.time()
+
+    # Perform the classical computation (e.g., matrix-vector multiplication)
+    result = vqe.compute_minimum_eigenvalue(operator=hamiltonian)
+    ansatz_params = result.optimal_point
+    classical_result = np.exp(-1j * hamiltonian.to_matrix()) @ ansatz.bind_parameters(ansatz_params)
+
+    end_time_classical = time.time()
+    calculation_time_classical = end_time_classical - start_time_classical
+
+    # Measure the time it takes to perform the quantum VQE computation
+    start_time_quantum = time.time()
+
+    # Run the VQE algorithm
+    result = vqe.compute_minimum_eigenvalue(operator=hamiltonian)
+
+    end_time_quantum = time.time()
+    calculation_time_quantum = end_time_quantum - start_time_quantum
+
+    # Compare the computing times
+    comparison = calculation_time_classical / calculation_time_quantum
+
+    # Return the computation results as a JSON response
+    return Response({
+        'classical_time': calculation_time_classical,
+        'quantum_time': calculation_time_quantum,
+        'comparison': comparison
+    }, status=status.HTTP_200_OK)
--- server/server/urls.py
+++ server/server/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from api.views import index
+from api.views import benchmark
+from api.views import generate_charts
 from django.conf.urls.static import static
 from django.conf import settings
 
 
 urlpatterns = [
     path('api/get-plots/', index),
+    path('api/benchmark/', benchmark),
+    path('api/generate-charts/', generate_charts),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
