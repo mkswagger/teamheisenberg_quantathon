3189bfab5a293646889a5f544d082af8e0fd3826
ishaan <ishaankhurana123@gmail.com>
2023-08-22 12:17:02 +0530

    feat: added render domain to whitelist

--- client/web/quantathon/next.config.js
+++ client/web/quantathon/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["127.0.0.1"], // Add any other domains as needed
+    domains: ["127.0.0.1", "quest-grapgh-gen.onrender.com"], // Add any other domains as needed
   },
 };
 
